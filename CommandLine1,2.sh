131bd7a3d746985b9d4f9ef98bdb97688ab50095
Tuba Siddiqui <57428557+TubaSid@users.noreply.github.com>
2023-01-03 17:36:23 +0100

    Updated part 3 and added 'head - 10' in part 2

--- CommandLine1,2.sh
+++ CommandLine1,2.sh
@@ -1,8 +1,6 @@
 echo "The most popular pair of heroes (often appearing together in the comics): "
-sort  hero-network.csv | uniq -cd | sort -nr| head -1
+awk -F, '{if($1 != $2) print $0}' hero-network.csv | uniq -c | sort -nr| head -1 
 echo "Number of comics per hero in ascending order: "
-awk -F, '{print $1}' edges.csv | uniq -c | sort -n
+awk -F, '{print $1}' edges.csv | uniq -c | sort -nr | head -10
 echo "The average number of heroes in comics: "
-awk -F, '{print $2}' edges.csv | uniq -cd | sort -n
-awk -F, '{print $2}' edges.csv | uniq -cd | sort -n > heros.csv
-cat heros.csv | wc -l
+awk -F, '{heros[$2]++} END {for (i in heros){sum = sum + heros[i]; print i, sum/length(heros)}}' edges.csv | head -10
