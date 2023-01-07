1bb44ba0069d04f527ca078a67aca93181b6ef14
Tuba Siddiqui <57428557+TubaSid@users.noreply.github.com>
2023-01-07 23:03:38 +0100

    Updated Command Line

--- CommandLine.sh
+++
@@ -1,6 +0,0 @@
-echo "The most popular pair of heroes (often appearing together in the comics): "
-awk -F, '{if($1 != $2) print $0}' hero-network.csv | uniq -c | sort -nr| head -1 
-echo "Number of comics per hero in ascending order: "
-awk -F, '{print $1}' edges.csv | uniq -c | sort -nr | head -10
-echo "The average number of heroes in comics: "
-awk -F, '{heros[$2]++} END {for (i in heros){sum = sum + heros[i]; print i, sum/length(heros)}}' edges.csv | head -10
